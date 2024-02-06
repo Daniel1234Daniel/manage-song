e67120ac1fc5b7051a251bb0656428baabeb68bf
Daniel T/yohannes <dtekleyohannes@gmail.com>
2024-02-06 21:46:20 +0300

    cors added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const songRoutes = require('./routes/songRoutes');
-
+const cors = require('cors');
 const app = express();
+app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
